dd8b057f17c1c9d0cf43ef4d57d07798baa71a70
Omar Khan <omakhan98@gmail.com>
2023-10-10 17:54:30 -0500

    added side nav to navbar

--- src/pages/Home.js
+++ src/pages/Home.js
@@ -1,11 +1,35 @@
 import React, { useEffect } from "react";
+import styled from "styled-components";
 
 import HeadingText from "../components/HeadingText";
+import SearchBox from "../UI/SearchBox";
+
+const SearchBoxCenter = styled.div`
+  display: flex;
+  justify-content: center;
+  width: 100%;
+`;
+
+const SearchHint = styled.p`
+  display: flex;
+  justify-content: center;
+  font-size: 0.8rem;
+  color: #a0a0a0;
+`;
 
 function Home() {
   return (
     <>
       <HeadingText />
+      <SearchBoxCenter>
+        <SearchBox
+          id="outlined-basic"
+          label="Search Type of Nonprofit Organizations"
+          variant="outlined"
+          style={{ margin: "1.5rem 0rem 0 0rem" }}
+        />
+      </SearchBoxCenter>
+      <SearchHint>Enter keyword for example: cancer, animal</SearchHint>
     </>
   );
 }
